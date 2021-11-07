fc055718a4d83e6e4a4f8911329f85e9be41355b
Grant <grant.studio.2021@gmail.com>
2021-11-06 23:54:57 -0400

    First version complete.

---
+++ WebContent/assets/script.js
@@ -0,0 +1,71 @@
+const container = document.querySelector('.container')
+const navbar = document.querySelector('.navbar')
+
+const windowHeight = window.innerHeight;
+
+container.style.height = windowHeight - navbar.offsetHeight + 'px';
+
+const HTMLparser = new DOMParser();
+
+var htmlMixedEditor = CodeMirror.fromTextArea(htmleditor, {
+    mode: "htmlmixed",
+    lineNumbers: true,
+    lineWrapping: true,
+    theme: "dracula"
+});
+
+var cssEditor = CodeMirror.fromTextArea(csseditor, {
+    mode: "css",
+    lineNumbers: true,
+    lineWrapping: true,
+    theme: "dracula"
+});
+
+var result = CodeMirror.fromTextArea(resulteditor, {
+    mode: "htmlmixed",
+    lineNumbers: true,
+    lineWrapping: true,
+    theme: "dracula",
+    readOnly: true
+});
+
+htmlMixedEditor.on('change', changeListener)
+cssEditor.on('change', changeListener)
+
+function changeListener() {
+    try {
+        const html = htmlMixedEditor.getValue()
+        const css = cssEditor.getValue()
+        const parsed = cssparser.parse(css)
+        if (parsed) {
+            const parsedJSON = parsed.toJSON('simple');
+            const parsedHTML = HTMLparser.parseFromString(html, 'text/html');
+            window.e = parsedHTML;
+            window.y = parsedJSON
+            parsedJSON.value.forEach(e => {
+                if (e.type == 'rule') {
+                    const keys = Object.keys(e.declarations);
+                    e.selectors.forEach(selector => {
+                        parsedHTML.body.querySelectorAll(selector).forEach(element => {
+                            keys.forEach(prop => element.style[prop] = e.declarations[prop]);
+                        })
+                    })
+                }else if(e.type == '@import'){
+                    let matches = e.value.match(/'|"/g);
+                    let firstQuoteIndex = e.value.indexOf(matches[0])
+                    let lastQuoteIndex = e.value.lastIndexOf(matches[matches.length-1]);
+                    const url = e.value.substring(firstQuoteIndex + 1, lastQuoteIndex);
+                    var linkElement = document.createElement('link')
+                    linkElement.href = url;
+                    linkElement.rel = "stylesheet";
+                    parsedHTML.head.appendChild(linkElement);
+                }
+            })
+
+            result.getDoc().setValue(html_beautify(parsedHTML.documentElement.outerHTML))
+        }
+    } catch (e) {
+        console.log(e)
+        result.getDoc().setValue('...')
+    }
+}
